b77aeb015f17640cb77502a7725e48df5b5e79b5
ahan <ahanraj03@gmail.com>
2025-07-17 10:19:32 +0530

    initial

---
+++ FirstSpringBootProject/src/main/java/com/example/FirstSpring/ServiceIMPL/studentServiceIMPL.java
@@ -0,0 +1,32 @@
+package com.example.FirstSpring.ServiceIMPL;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.FirstSpring.Model.StudentDetails;
+import com.example.FirstSpring.service.StudentService;
+import com.example.FirstSpring.Repository.StudentRepository;
+@Service
+public class studentServiceIMPL implements StudentService {
+	@Autowired
+	
+	StudentRepository studentRepository;
+	
+
+	@Override
+	public void saveStudent(StudentDetails studentDetails) {
+		// TODO Auto-generated method stub
+		studentRepository.save(studentDetails);
+		
+	}
+
+
+	@Override
+	public List<StudentDetails> getAllstudent() {
+		// TODO Auto-generated method stub
+		return studentRepository.findAll();
+	}
+
+}
